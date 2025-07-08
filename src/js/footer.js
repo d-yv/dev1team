7507d2bc52d72ec0a1ddba299bbb34c3285c9a5b
DzyadzoMM <m.dziadzo@gmail.com>
2025-07-08 08:35:41 +0300

    Dynamic change of styles

--- src/js/footer.js
+++ src/js/footer.js
@@ -9,17 +9,28 @@ const emailInput = form.elements.email;
 
 form.addEventListener('input', (event) => {
     if (event.target.name === 'email') {
-        formData.email = event.target.value.trim();}
+        formData.email = event.target.value.trim();
+    }
 });
 
 form.addEventListener('submit', (event) => {
+  const errorBorderColor = '#AD0000';
+  const errorColor= '#AD0000';
+  
+  emailInput.style.borderColor ='rgba(11, 5, 0, 0.60)';
+  emailInput.style.color = '#0B0500';
+  
     event.preventDefault();
     if (formData.email === "") {
       showMessage('red', 'Fill please all fields.');
+      emailInput.style.borderColor = errorBorderColor;
+      emailInput.style.color = errorColor;
         return;
     }
     if (!emailInput.checkValidity()){
       showMessage('red', 'Please enter a valid email address.');
+      emailInput.style.borderColor = errorBorderColor;
+      emailInput.style.color = errorColor;
       return;
     }
     console.log(formData);
